3fe126fa4fbed5bc919a90580bc562ce4b2596f3
GopaniRajakaruna <gopanisathruwani@gmail.com>
2026-01-15 13:16:33 +0530

    Update fetch_file.py

--- fetch_file.py
+++ fetch_file.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://raw.githubusercontent.com/ASHINI04/Hello-python/main/hello.py"
+url = "https://raw.githubusercontent.com/GopaniRajakaruna/Hello-python/main/hello.py"
 response = requests.get(url)
 
 print(response.text)
